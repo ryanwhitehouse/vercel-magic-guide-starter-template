1dab29916fa6f9e01dd568b6eec006e36b4931a3
Ryan Whitehouse <rwhitehouse.rw@gmail.com>
2024-04-06 23:22:38 +0200

    Add magic authentication

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,32 @@
-
+import { useContext } from 'react';
+import { UserContext } from '@/lib/UserContext';
+import { magic } from '@/lib/magic';
+import { useRouter } from 'next/router';
 
 export default function Dashboard() {
+  const [user, setUser] = useContext(UserContext);
+  // Create our router
+  const router = useRouter();
+  console.log({user})
+  
+  const logout = () => {
+    // Call Magic's logout method, reset the user state, and route to the login page
+    magic.user.logout().then(() => {
+      setUser({ user: null });
+      router.push('/login');
+    });
+  };
 
   return (
     <>
-
+      {user?.issuer && (
+        <>
+          <h1>Your current rentals</h1>
+          <h2>Hi {user?.name}</h2>
+          <p>{user.email}</p>
+          <button onClick={logout}>Logout</button>
+        </>
+      )}
     </>
   );
 }
--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,28 @@
-
+import { useContext, useState } from 'react';
+import { UserContext } from '@/lib/UserContext';
 
 export default function Login() {
+  // Allows us to access the user state and update it within this page
+  const [user, setUser] = useContext(UserContext);
+  // Our email state that we'll use for our login form
+  const [email, setEmail] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // We'll fill in the rest of this later
+  };
 
   return (
-
+    <form onSubmit={handleLogin}>
+      <label htmlFor="email">Email</label>
+      <input
+        name="email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type="submit">Send Magic Link</button>
+    </form>
   );
 }
